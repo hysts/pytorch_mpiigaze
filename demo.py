9a9507f869cdee95ed4751e55f83e36993fb8029
hysts <hysts@users.noreply.github.com>
2019-08-20 05:52:38 +0900

    Update

--- demo.py
+++ demo.py
@@ -31,10 +31,22 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, gaze_estimator.camera.height)
 
     QUIT_KEYS = {27, ord('q')}
+    show_bbox = True
+    show_head_pose = True
+    show_landmarks = True
+    show_template_model = False
     while True:
         key = cv2.waitKey(1) & 0xff
         if key in QUIT_KEYS:
             break
+        elif key == ord('b'):
+            show_bbox = not show_bbox
+        elif key == ord('l'):
+            show_landmarks = not show_landmarks
+        elif key == ord('h'):
+            show_head_pose = not show_head_pose
+        elif key == ord('t'):
+            show_template_model = not show_template_model
 
         ok, frame = cap.read()
         if not ok:
@@ -45,24 +57,31 @@ def main():
         for face in faces:
             gaze_estimator.estimate_gaze(frame, face)
 
-            euler_angles = face.head_pose_rot.as_euler('XYZ', degrees=True)
-            pitch, yaw, roll = face.change_coordinate_system(euler_angles)
-            logger.info(
-                f'[head] pitch: {pitch:.2f}, yaw: {yaw:.2f}, roll: {roll:.2f}, '
-                f'distance: {face.distance:.2f}')
             for key in ['reye', 'leye']:
                 eye = getattr(face, key)
                 pitch, yaw = np.rad2deg(eye.vector_to_angle(eye.gaze_vector))
                 logger.info(f'[{key}] pitch: {pitch:.2f}, yaw: {yaw:.2f}')
 
-            visualizer.draw_bbox(face.bbox)
-            visualizer.draw_points(face.landmarks, color=(0, 255, 255), size=1)
-            # Draw the transformed template model
-            visualizer.draw_3d_points(face.model3d,
-                                      color=(255, 0, 525),
-                                      size=1)
-            # Draw the axes of the model coordinate system
-            visualizer.draw_model_axes(face, lw=2)
+            if show_bbox:
+                visualizer.draw_bbox(face.bbox)
+            if show_head_pose:
+                # Draw the axes of the model coordinate system
+                visualizer.draw_model_axes(face, lw=2)
+
+                euler_angles = face.head_pose_rot.as_euler('XYZ', degrees=True)
+                pitch, yaw, roll = face.change_coordinate_system(euler_angles)
+                logger.info(f'[head] pitch: {pitch:.2f}, yaw: {yaw:.2f}, '
+                            f'roll: {roll:.2f}, distance: {face.distance:.2f}')
+            if show_landmarks:
+                visualizer.draw_points(face.landmarks,
+                                       color=(0, 255, 255),
+                                       size=1)
+            if show_template_model:
+                # Draw the transformed template model
+                visualizer.draw_3d_points(face.model3d,
+                                          color=(255, 0, 525),
+                                          size=1)
+
             # Draw the gaze vectors
             length = config.demo.gaze_visualization_length
             for key in gaze_estimator.EYE_KEYS:
