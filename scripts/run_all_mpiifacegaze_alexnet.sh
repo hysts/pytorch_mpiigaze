3cabb9a5098a2f6fc8d8ed1fa52fac78f23440c1
Shaikh Islam <shaikhalvee@gmail.com>
2024-03-15 02:30:40 -0400

    requirements removed so that vulnerables will be updated

--- scripts/run_all_mpiifacegaze_alexnet.sh
+++ scripts/run_all_mpiifacegaze_alexnet.sh
@@ -9,13 +9,13 @@ exp_root_dir=experiments/mpiifacegaze/${arch}/exp${exp_id}/
 for test_id in {0..14}; do
     CUDA_VISIBLE_DEVICES=${devices} python -u train.py \
         --config configs/mpiifacegaze/${arch}_train.yaml \
-        train.test_id ${test_id} \
+        train.test_id "${test_id}" \
         train.output_dir "${exp_root_dir}"
 
-    exp_dir=${exp_root_dir}/$(printf %02d ${test_id})
+    exp_dir=${exp_root_dir}/$(printf %02d "${test_id}")
     CUDA_VISIBLE_DEVICES=${devices} python -u evaluate.py \
         --config configs/mpiifacegaze/${arch}_eval.yaml \
-        test.test_id ${test_id} \
+        test.test_id "${test_id}" \
         test.checkpoint "${exp_dir}"/checkpoint_0015.pth \
         test.output_dir "${exp_dir}"/eval
 done
